a7aab86548305a1e04d5d8e049c9eed27f4be1e9
Kauê Ruas <112980882+kaueruas@users.noreply.github.com>
2023-11-26 19:10:43 -0300

    alterações layout

--- crimes1.py
+++ crimes1.py
@@ -40,44 +40,35 @@ with left_column:
 media_ocorrencias = df.groupby('Tipo_crime')['Ocorrencias'].mean().sort_values(ascending=False).reset_index()
 
 with right_column:
-    fig = px.bar(media_ocorrencias, x='Ocorrencias', y='Tipo_crime', orientation='h',
+    fig1 = px.bar(media_ocorrencias, x='Ocorrencias', y='Tipo_crime', orientation='h',
                  title='Média de Ocorrências por Tipo de Crime',
                  labels={'Ocorrencias': 'Média de Ocorrências', 'Tipo_crime': 'Tipo de Crime'},
                  height=520)
-    st.plotly_chart(fig)
+    st.plotly_chart(fig1)
 
 agrupar_data = df.groupby("Data")["Ocorrencias"].mean().reset_index()
-fig=px.line(agrupar_data, x="Data", y="Ocorrencias" , labels={"Ocorrencias":"Ocorrências"},title="Ocorrências de crimes ao longo dos anos" )
+fig2=px.line(agrupar_data, x="Data", y="Ocorrencias" , labels={"Ocorrencias":"Ocorrências"},title="Ocorrências de crimes ao longo dos anos" )
 col1, col2, col3 = st.columns([1, 3, 1])  
 with col2:
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig2, use_container_width=True)
 
 
 st.markdown('''---''')
-
-media_por_estado = df.groupby('Uf')['Ocorrencias'].mean().reset_index()
-media_por_estado = media_por_estado.sort_values(by='Ocorrencias', ascending=False)
-fig = px.area(media_por_estado, x='Ocorrencias', y='Uf',orientation="h", title='Média de Ocorrências por Estado',
-              labels={'Uf': 'Estado', 'Ocorrencias': 'Média de Ocorrências'}, width=1100)
-
-st.plotly_chart(fig)
-
 
 dados_crimes = pd.DataFrame(df)
 dados_sp = dados_crimes.copy()
 dados_sp['Data'] = pd.to_datetime(dados_sp['Data'])
 dados_sp.loc[:,'ano'] = dados_sp['Data'].dt.year
 crimes_por_ano = dados_sp.groupby(['ano', 'Tipo_crime'])['Ocorrencias'].mean().reset_index()
-fig = px.line(crimes_por_ano, x='ano', y='Ocorrencias', color='Tipo_crime', title='Evolução dos Crimes no Brasil (2014-2022)',labels={'Ocorrencias': 'Ocorrências', 'ano': 'Ano', 'Tipo_crime': 'Tipo de Crime'},width=1000)
-st.plotly_chart(fig)
-#
-dados_crimes = pd.DataFrame(df)
-dados_sp = dados_crimes[dados_crimes['Uf'] == 'Maranhão'].copy()
-dados_sp['Data'] = pd.to_datetime(dados_sp['Data'])
-dados_sp.loc[:,'ano'] = dados_sp['Data'].dt.year
-crimes_por_ano = dados_sp.groupby(['ano', 'Tipo_crime'])['Ocorrencias'].mean().reset_index()
-fig = px.line(crimes_por_ano, x='ano', y='Ocorrencias', color='Tipo_crime', title='Evolução dos Crimes Por Estado (2014-2022)', labels={'Ocorrencias': 'Ocorrências', 'ano': 'Ano', 'Tipo_crime': 'Tipo de Crime'},width=1000)
-st.plotly_chart(fig)
+fig3 = px.line(crimes_por_ano, x='ano', y='Ocorrencias', color='Tipo_crime', title='Evolução dos Crimes no Brasil (2014-2022)',labels={'Ocorrencias': 'Ocorrências', 'ano': 'Ano', 'Tipo_crime': 'Tipo de Crime'}, width=1200)
+st.plotly_chart(fig3)
+
+media_por_estado = df.groupby('Uf')['Ocorrencias'].mean().reset_index()
+media_por_estado = media_por_estado.sort_values(by='Ocorrencias', ascending=False)
+fig4 = px.area(media_por_estado, x='Ocorrencias', y='Uf',orientation="h", title='Média de Ocorrências por Estado',
+              labels={'Uf': 'Estado', 'Ocorrencias': 'Média de Ocorrências'})
+
+st.plotly_chart(fig4)
 
 
     
